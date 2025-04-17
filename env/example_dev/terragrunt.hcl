a8a31764a7e55b21a7e48fbe4ac9d2edc307bd97
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-04-17 11:34:40 +0900

    feat: example_dev 추가 및 data.tf 수정

---
+++ env/example_dev/terragrunt.hcl
@@ -0,0 +1,21 @@
+locals {
+  file_path = "../../../config"
+  env       = "dev"
+}
+
+terraform {
+  source = "../../terraform"
+}
+
+remote_state {
+  backend = "gcs"
+  config = {
+    bucket         = "${local.env}-for-example-state-bucket"
+    prefix         = "terraform/state"
+  }
+}
+
+inputs = {
+  file_path = local.file_path
+  env       = local.env
+}
--- terraform/data.tf
+++ terraform/data.tf
@@ -1,8 +1,10 @@
 data "google_client_config" "current" {}
 
-/*
-* 생성한 GKE에 연결하기 위한 data resource - Main 전용
-*/
+######################################
+## main
+######################################
+
+# 생성한 GKE에 연결하기 위한 data resource
 data "google_container_cluster" "gke" {
   count = lookup(local.yaml.yaml_data.provider.main, "gke_name", "") != "" ? 1 : 0
 
@@ -11,9 +13,7 @@ data "google_container_cluster" "gke" {
   location = local.yaml.yaml_data.provider.main.gke_location
 }
 
-/*
-* Ingress-Nginx의 External IP를 가져오기 위한 data resource
-*/
+# Ingress-Nginx의 External IP를 가져오기 위한 data resource
 data "kubernetes_service" "nginx_ingress" {
   count = lookup(local.yaml.yaml_data.provider.main, "nginx_service_name", "") != "" ? 1 : 0
 
@@ -25,9 +25,7 @@ data "kubernetes_service" "nginx_ingress" {
   depends_on = [helm_release.this]
 }
 
-/*
-* Redis_Master의 External IP를 가져오기 위한 data resource
-*/
+# Redis_Master의 External IP를 가져오기 위한 data resource
 data "kubernetes_service" "redis_master" {
   count = lookup(local.yaml.yaml_data.provider.main, "redis_service_name", "") != "" ? 1 : 0
 
